875bd5086c138ed3fa420b13ebb2a7e21d10c66e
paaavleee <kurtanidzepavle@gmail.com>
2022-12-02 03:28:03 +0400

    5sect

--- app.js
+++ app.js
@@ -3,42 +3,41 @@ const skilTran = document.querySelector(".skil-tran");
 const sections = document.querySelector(".asd");
 const progressSpan = document.querySelector(".progress-span");
 const phoneImg = document.querySelectorAll(".phone-img");
-
-
+const recomenUser = document.querySelectorAll(".recomen-user");
+const rcnBulet = document.querySelectorAll(".rcn-bulet");
 
 let activeIndex = 0;
 
 function activeIndecFun() {
-    slider.forEach((slide,index)=>{
-        if (activeIndex === index) {
-            slide.classList.add("active");
-        }else{
-            slide.classList.remove("active");
-        }
-    })
-    
+  slider.forEach((slide, index) => {
+    if (activeIndex === index) {
+      slide.classList.add("active");
+    } else {
+      slide.classList.remove("active");
+    }
+  });
 }
 
 function nextSlide() {
-	// console.log("next slide");
+  // console.log("next slide");
 
-	if (activeIndex === slider.length - 1) {
-		activeIndex = 0;
-	} else {
-		activeIndex++;
-	}
+  if (activeIndex === slider.length - 1) {
+    activeIndex = 0;
+  } else {
+    activeIndex++;
+  }
 
-	activeIndecFun();
+  activeIndecFun();
 }
-activeIndecFun()
+activeIndecFun();
 
 function startAutoSlidingFn() {
-	sliderIntervalId = setInterval(nextSlide, 5000);
+  sliderIntervalId = setInterval(nextSlide, 5000);
 }
 
 function initSlider() {
-    activeIndecFun();
-    startAutoSlidingFn();
+  activeIndecFun();
+  startAutoSlidingFn();
 }
 initSlider();
 
@@ -48,11 +47,38 @@ initSlider();
 //     progressSpan.forEach((span) => {
 //         span.style.width = span.dataset.width;
 //     });
-    
+
 // };
 
 // window.addEventListener('scroll',() => {
 //     console.log('scolled')
 //     scrollElement();
 // })
+let activeIn = 0;
 
+function activeUser() {
+  recomenUser.forEach((elem, index) => {
+    if (activeIn === index) {
+      elem.classList.add("appear");
+    } else {
+      elem.classList.remove("appear");
+    }
+  });
+}
+
+function nextBulet() {
+  rcnBulet.forEach((el, index) => {
+    el.addEventListener("click", () => {
+      console.log(11111);
+      activeIn = index;
+
+      activeUser(rcnBulet);
+    });
+  });
+}
+
+function recomenNextUser() {
+  activeUser();
+  nextBulet();
+}
+recomenNextUser();
